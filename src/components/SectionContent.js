2f20a07f1310e9b6ad212edd3ce89fb10ad517da
Stackbit Code Editor <support@stackbit.com>
2021-02-22 22:07:34 +0000

    SectionContent.js: changed

--- src/components/SectionContent.js
+++ src/components/SectionContent.js
@@ -8,12 +8,12 @@ export default class SectionContent extends React.Component {
         let section = _.get(this.props, 'section', null);
         return (
             <section id={_.get(section, 'section_id', null)} className="content">
+                {_.get(section, 'image', null) && (<img className="inline-image" src={withPrefix(_.get(section, 'image', null))} alt={_.get(section, 'image_alt', null)} />)}
                 {_.get(section, 'content', null) && (
                 <div className="copy">
                     {markdownify(_.get(section, 'content', null))}
                 </div>
                 )}
-                {_.get(section, 'image', null) && (<img className="inline-image" src={withPrefix(_.get(section, 'image', null))} alt={_.get(section, 'image_alt', null)} />)}
             </section>
         );
     }
